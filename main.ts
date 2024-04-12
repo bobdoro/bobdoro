f800b53941a072733efee2adc539a31380999c73
bobdoro <166851857+bobdoro@users.noreply.github.com>
2024-04-12 13:46:21 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    basic.showString("oi, tudo bem")
+    basic.showIcon(IconNames.Chessboard)
+    basic.showString("me chamo maria esther")
+    basic.showIcon(IconNames.Heart)
+    basic.showString("mas pode me chamar de tete")
+    basic.showIcon(IconNames.Cow)
+    basic.showString("eu tenho 15 anos")
+})
